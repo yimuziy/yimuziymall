cda092326ee1fac119a7e9db9b342b1b7fb64ebe
yimuziy <70566352+yimuziy@users.noreply.github.com>
2021-03-16 13:11:59 +0800

    debug

--- mall-product/src/test/java/com/yimuziy/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/yimuziy/mall/product/MallProductApplicationTests.java
@@ -98,8 +98,8 @@ class MallProductApplicationTests {
 ////        // Endpoint以杭州为例，其它Region请按实际情况填写。
 ////        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 ////        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-////        String accessKeyId = "LTAI4G51DA6sWEJQ1LXWmrhL";
-////        String accessKeySecret = "IgAenHT03454WDc87JOa4RVLteZSYl";
+////        String accessKeyId = "LTAI4G51DLXWmrhL";
+////        String accessKeySecret = "IgAenHVLteZSYl";
 ////
 ////        // 创建OSSClient实例。
 ////        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
